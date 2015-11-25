3b0a001179e15a9f6ef1e757d9861ffe13107259
Andrés Mañas <andres.manas-manas@nokia.com>
2015-11-25 18:33:16 +0100

    ad

--- src/test/java/amanas17/java/md/p2_1/GeneticSearchTest.java
+++ src/test/java/amanas17/java/md/p2_1/GeneticSearchTest.java
@@ -5,23 +5,29 @@ import org.junit.Test;
 import weka.attributeSelection.GeneticSearch;
 
 public class GeneticSearchTest extends BaseTest {
+	@Test
+	public void ConsistencySubsetEval() throws Exception {
+		GeneticSearch s = new GeneticSearch();
+		ConsistencySubsetEval e = new ConsistencySubsetEval();
+		selectAttributes(s, e);
+	}
 
-    /**
-     * Igual de mal
-     */
-    @Test
-    public void GeneticSearchWithStartSet() throws Exception {
-        GeneticSearch gs = new GeneticSearch();
-        gs.setStartSet("1,4");
-        selectAttributes(gs, null);
-    }
+	/**
+	 * Igual de mal
+	 */
+	@Test
+	public void GeneticSearchWithStartSet() throws Exception {
+		GeneticSearch gs = new GeneticSearch();
+		gs.setStartSet("1,4");
+		selectAttributes(gs, null);
+	}
 
-    /**
-     * Fatal - no entiendo por qu�
-     */
-    @Test
-    public void GeneticSearchDefault() throws Exception {
-        GeneticSearch gs = new GeneticSearch();
-        selectAttributes(gs, null);
-    }
+	/**
+	 * Fatal - no entiendo por qu�
+	 */
+	@Test
+	public void GeneticSearchDefault() throws Exception {
+		GeneticSearch gs = new GeneticSearch();
+		selectAttributes(gs, null);
+	}
 }
